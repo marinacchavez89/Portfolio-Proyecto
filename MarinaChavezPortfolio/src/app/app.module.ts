2793e0061ccd0cea523d3e82a885a88ccce1ff88
marinacchavez89 <marinachavez89@gmail.com>
2022-05-04 18:31:44 -0300

    actualizacion cambios portfolio angular

--- MarinaChavezPortfolio/src/app/app.module.ts
+++ MarinaChavezPortfolio/src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { ExperiencialaboralComponent } from './componentes/experiencialaboral/experiencialaboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    ExperiencialaboralComponent,
+    EducacionComponent,
+    ProyectosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
